74a1ff3b21010d437b8a0a9f703832370d8e3055
viruelz <viruelz@gmail.com>
2017-06-01 21:53:21 -0300

    desfazendo umas merda e criando novas çkçkçkç

--- ProjetoPokemon/Controllers/PokemonModelController.cs
+++ ProjetoPokemon/Controllers/PokemonModelController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Model.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +13,34 @@ namespace ProjetoPokemon.Controllers
         // GET: PokemonModel
         public ActionResult findAll()
         {
+            ViewBag.ListTime = findAllPokeById();
             return View();
         }
+
+        public ICollection<PokemonModel> findAllPokeById()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                var baseUrl = "endereço WS";
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/jason"));
+                HttpResponseMessage response = client.GetAsync("PokemonModel'view'").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<ICollection<PokemonModel>>().Result;
+                }
+
+                return null;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+        }
     }
+  
 }
--- ProjetoPokemon/Controllers/TimeModelController.cs
+++ ProjetoPokemon/Controllers/TimeModelController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Model.Model;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
 using System.Web.Mvc;
 
 namespace ProjetoPokemon.Controllers
@@ -9,6 +9,12 @@ namespace ProjetoPokemon.Controllers
     public class TimeModelController : Controller
     {
         // GET: TimeModel
+        public ActionResult mostrarTime()
+        {
+            ViewBag.ListTime = findTimeById();
+            return View();
+        }
+
         public ActionResult montarTime()
         {
             return View();
@@ -17,6 +23,31 @@ namespace ProjetoPokemon.Controllers
         public ActionResult deletarTime()
         {
             return View();
+        }
+
+        public ICollection<TimeModel> findTimeById()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                var baseUrl = "endereço WS";
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/jason"));
+                HttpResponseMessage response = client.GetAsync("TimeModel'view'").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<ICollection<TimeModel>>().Result;
+                }
+
+                return null;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
         }
     }
 }
